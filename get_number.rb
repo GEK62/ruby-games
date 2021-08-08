f3ee6c6c587336f23aa0325986d6870f82ce7357
Phoenix#^# <abasovwork@gmail.com>
2021-08-08 22:39:45 +0300

    new structure

--- get_number.rb
+++ get_number.rb
@@ -1,5 +1,5 @@
-# Get My Number Game
-# Written by: Gek!
+# Игра угадай число.
+# Автор: Gek!
 
 puts "Welcome to 'Get My Number!'"
 print "What's your name? "
@@ -10,5 +10,30 @@ name = input.chomp
 
 puts "Welcome, #{name}!"
 
-puts 42.upcase
+# Получение имени игрока и вывод приветствия.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
 
+# Отслеживание количества попыток.
+num_guesses = 0
+
+# Признак продолжения игры.
+guessed_it = false
+
+# Сохранение случайного числа.
+puts "You've got #{10 - num_guesses} guesses left."
+print "Make a guess: "
+guess = gets.to_i
+
+# Сравнение введенного числа с загаданным
+# и вывод соответсвующего сообщения
+if guess < target
+    puts "Oops. Your guess was LOW."
+elsif guess > target
+    puts "Oops. Your guess was HIGH."
+elsif guess == target
+    puts "Good job, #{name}!"
+    puts "You guessed my number #{num_guesses} guesses!"
+    guessed_it = true
+end
