41966d8a2b5bf636e993dda516cae5e42a10845e
Phoenix#^# <abasovwork@gmail.com>
2021-08-08 18:31:05 +0300

    First init

---
+++ get_number.rb
@@ -0,0 +1,14 @@
+# Get My Number Game
+# Written by: Gek!
+
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+
+input = gets
+
+name = input.chomp
+
+puts "Welcome, #{name}!"
+
+puts 42.upcase
+
